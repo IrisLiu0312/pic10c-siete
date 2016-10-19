f3f443d6f8ae091198475bc52c8a3f4ec5c59ee4
Iris Liu <bloodiris0312@ucla.edu>
2016-10-19 11:31:33 -0700

    Progression + isBust()
    Added game progression summary and the isBust() function.

--- hw7.cpp
+++ hw7.cpp
@@ -8,16 +8,47 @@
 using namespace std;
 
 // Global constants (if any)
-
+const double THRESHOLD = 7.5; //Max value before busting
 
 // Non member functions declarations (if any)
-
+/*Checks whether the hand has busted.
+@param hand to check
+@return true if busted, else false
+*/
+bool isBust(const Hand& hand);
 
 // Non member functions implementations (if any)
+bool isBust(const Hand& hand){
+	if(hand.getValue() > THRESHOLD)
+		return true;
+	return false;
+}
 
-
-// Stub for main
 int main(){
-   /* --STATEMENTS-- */
+   /*Game progression:
+   	Rules
+   		> 7.5 = bust
+	Beginning:
+		Player $ = 100
+		Dealer $ = 900
+	Every Round
+		P makes a bet
+		P draw card, D draw card, P cards are shown
+		Another card? (draw and repeat until total < 7.5 || no draw)
+		Show dealer's cards (who draws only if total < 5.5)
+	Calculation
+		(P1 > D) || (D bust)
+			P wins, P.$ += bet, D.$ -= bet
+		(D > P) || (P bust)
+			P loses, P.$ -= bet, D.$ += bet
+		(P bust) && (D bust) //house advantage
+			P loses, P.$ -= bet, D.$ += bet
+		(P == D) && (no one bust)
+			Tie, nothing happens
+	End game
+		(P.$ == 0) || (D.$ == 0)
+		Input validation may be required.
+	*/
+	
    return 0;
 }
